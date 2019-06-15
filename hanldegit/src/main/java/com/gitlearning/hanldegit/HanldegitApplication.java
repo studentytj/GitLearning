fb9d55fdbcd899a55461cc1edf645b5b7f863ba8
studentytj <studentytj@163.com>
2019-06-15 17:21:02 +0800

    first initial commit

---
+++ hanldegit/src/main/java/com/gitlearning/hanldegit/HanldegitApplication.java
@@ -0,0 +1,13 @@
+package com.gitlearning.hanldegit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class HanldegitApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(HanldegitApplication.class, args);
+    }
+
+}
